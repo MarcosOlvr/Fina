e0190daf129615ec5eb9f89818608b850ae1cfe5
Marcos <marquinhos.dev@gmail.com>
2024-05-30 17:30:17 -0300

    ++ db | ++ api/handlers

--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -1,4 +1,15 @@
+using Fina.Api.Data;
+using Fina.Api.Handlers;
+using Fina.Core.Handlers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
